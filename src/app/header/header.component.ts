ce021559a1222988f092d3ab337d9a516f1de5d0
nika todua <alchimikos@gmail.com>
2025-05-06 11:49:49 +0400

    updating local storage setting

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -11,104 +11,129 @@ export class HeaderComponent {
   darkmodeicon:boolean = false
   darkmodeCount:number = 0
 
-   // dark mode event
-   darktoken:string = "Dark";
-   lighttoken:string = "Light";
+  // dark mode event
+  darktoken:string = "Dark";
+  lighttoken:string = "Light";
+
+  usetoken:string = 'English';
+  geotoken:string = 'Georgia';
+
+  localstorageArray:any = {
+    darkmode: `${this.darktoken}`,
+    language: `${this.usetoken}`
+  }
+  
   
   
   darkmodeevent(){
-    this.darkmodeCount++
+    this.darkmodeCount = (this.darkmodeCount + 1) % 2;
 
-    switch (this.darkmodeCount) {
-      case 1:
+    if (this.darkmodeCount === 1) {
       document.body.classList.add("light_mode");
       this.darkmodeicon = true;
-      this.darkmodeCount = 1;
-      localStorage.setItem('theme', this.lighttoken);
-      break;
-      case 2:
+      this.localstorageArray.darkmode = this.lighttoken
+      localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
+    } else {
       document.body.classList.remove("light_mode");
       this.darkmodeicon = false;
-      this.darkmodeCount = 0;
-      localStorage.setItem('theme', this.darktoken);
-      break;
+      this.localstorageArray.darkmode = this.darktoken
+      localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
     }
-    
   }
-
+  
   languageCount:number = 0
   flagimg:boolean = false
 
   // language
-  usetoken:string = 'USA';
-  geotoken:string = 'GEO';
   
-  
+
   languageEvent(){
-    this.languageCount++
-    if(this.languageCount === 1){
-      localStorage.setItem("language", this.geotoken)
-      this.flagimg = true
-      this.languageCount = 1
-      document.body.classList.add("geo-lang")
-      document.body.classList.remove("usa-lang")
-    } else if(this.languageCount === 2){
-      localStorage.setItem("language", this.usetoken)
-      this.flagimg = false
-      this.languageCount = 0
-      document.body.classList.add("usa-lang")
-      document.body.classList.remove("geo-lang")
+    this.languageCount = (this.languageCount + 1) % 2;
+
+    if (this.languageCount === 1) {
+      this.localstorageArray.language = this.geotoken
+      this.flagimg = true;
+      document.body.classList.add("geo-lang");
+      document.body.classList.remove("usa-lang");
+    } else {
+      this.localstorageArray.language = this.usetoken
+      this.flagimg = false;
+      document.body.classList.add("usa-lang");
+      document.body.classList.remove("geo-lang");
     }
-    location.reload()
+    localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
+    
+    location.reload();
   }
+  
   
   
   ngOnInit() {
     document.addEventListener("DOMContentLoaded", () => {
       
       // dark mode 
-      const storedTheme = localStorage.getItem('theme')
-
-      if(storedTheme?.includes(this.lighttoken)){
-        localStorage.setItem('theme', this.lighttoken)
+      const websettingstorage:any = localStorage.getItem("webSetting")
+      
+      localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
+      
+      if(JSON.parse(websettingstorage).darkmode?.includes(this.lighttoken)){
+        // localStorage.setItem('theme', this.lighttoken)
+        this.localstorageArray.darkmode = this.lighttoken
         document.body.classList.add("light_mode")
         this.darkmodeCount = 1
         this.darkmodeicon = true
-      } else if(storedTheme?.includes(this.darktoken)){
-        localStorage.setItem('theme', this.darktoken) 
+        localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
+      } else if(JSON.parse(websettingstorage).darkmode?.includes(this.darktoken)){
+        this.localstorageArray.darkmode = this.darktoken
+        // localStorage.setItem('theme', this.darktoken) 
         document.body.classList.remove("light_mode")
         this.darkmodeCount = 0
         this.darkmodeicon = false
+        localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
+      }else{
+        this.localstorageArray.darkmode = this.darktoken
+        // localStorage.setItem('theme', this.darktoken) 
+        document.body.classList.remove("light_mode")
+        this.darkmodeCount = 0
+        this.darkmodeicon = false
+        localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
       }
       
-
+      
       // language event
 
-      const storagelanguage = localStorage.getItem("language")
-
-      switch (storagelanguage) {
+      switch (JSON.parse(websettingstorage).language) {
         case this.usetoken:
           document.body.classList.add("usa-lang");
           document.body.classList.remove("geo-lang");
-          localStorage.setItem("language", this.usetoken);
+          // localStorage.setItem("language", this.usetoken);
+          this.localstorageArray.language = this.usetoken
           this.languageCount = 0;
           this.flagimg = false;
+          localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
           break;
         case this.geotoken:
           document.body.classList.add("geo-lang");
           document.body.classList.remove("usa-lang");
-          localStorage.setItem("language", this.geotoken);
+          // localStorage.setItem("language", this.geotoken);
+          this.localstorageArray.language = this.geotoken
           this.languageCount = 1;
+          localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
           this.flagimg = true;
           break;
         default:
           document.body.classList.add("usa-lang");
           document.body.classList.remove("geo-lang");
-          localStorage.setItem("language", this.usetoken);
+          this.localstorageArray.language = this.usetoken
+          // localStorage.setItem("language", this.usetoken);
           this.languageCount = 0;
+          localStorage.setItem('webSetting', JSON.stringify( this.localstorageArray ));
           this.flagimg = false;
           break;
-      }
+        }
+        
+        console.log( JSON.stringify( this.localstorageArray ) );
+        
       
     });
   }
